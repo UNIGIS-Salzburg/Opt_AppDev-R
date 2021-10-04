c684937b5810e0cc951276e79cd17c95794d7769
Neuwirth Christian <b1045498@is.sbg.ac.at>
2021-10-04 12:55:55 +0200

    solution exercise 1

---
+++ Solution_Exercise_DataWrangling1.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(knitr)
+
+# Read data from comma-separated csv
+fao_data <- read.csv("C:/Users/b1045498/Downloads/Production_Crops_Livestock_E_Africa/Production_Crops_Livestock_E_Africa_NOFLAG.csv", header = TRUE, sep = ",")
+
+fao_data %>% 
+  dplyr::select(Area, Item, Element, Unit, Y2019) %>%              #limit columns    
+  dplyr::filter(Item == "Sorghum", Element == "Production") %>%    #limit rows
+  dplyr::group_by(Area) %>%                                        #get production sum per country
+  dplyr::summarise(yield_2019 = sum(Y2019)) %>%
+  dplyr::arrange(-yield_2019) %>%                                  #sort descending by production in 2019
+  drop_na() %>%                                                    #remove rows including NAs
+  knitr::kable()
+
+#Note: When using pipe operators, there is no need to pass outputs as parameter functions!
+#(e.g. the parameter list of function drop_na() is empty)
