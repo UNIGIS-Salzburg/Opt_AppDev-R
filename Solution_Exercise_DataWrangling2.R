651505647ed71c9265b0502c38ee52fc92556b00
Neuwirth Christian <b1045498@is.sbg.ac.at>
2021-10-04 14:59:12 +0200

    solution exercise 2

---
+++ Solution_Exercise_DataWrangling2.R
@@ -0,0 +1,38 @@
+library(ggplot2) #new
+library(maps)    #new
+library(tidyverse)
+library(knitr)
+
+fao_data <- read.csv("C:/Users/b1045498/Downloads/Production_Crops_Livestock_E_Africa/Production_Crops_Livestock_E_Africa_NOFLAG.csv", header = TRUE, sep = ",")
+
+sorghum_production <- fao_data %>% 
+  dplyr::select(Area, Item, Element, Unit, Y2019) %>%                  
+  dplyr::filter(Item == "Sorghum", Element == "Production") %>%   
+  dplyr::group_by(Area) %>%                                        
+  dplyr::summarise(yield_2019 = sum(Y2019)) %>%
+  dplyr::arrange(-yield_2019)  
+
+#create data frame from maps dataset 'world'
+world_ctry <- map_data("world") 
+
+# left table
+africa_sorghum_production <- world_ctry %>%
+  # inner join
+  dplyr::left_join(
+    # right table
+    sorghum_production,
+    #columns to match
+    by = c("region" = "Area")
+  )
+
+ggplot(africa_sorghum_production, aes(x = long, y = lat)) +
+  geom_polygon(aes( group = group, fill = yield_2019)) +
+  scale_fill_gradient(
+    low = "white",
+    high = "red",
+    space = "Lab",
+    na.value = "grey80",
+    guide = "colourbar",
+    aesthetics = "fill"
+  )
+
