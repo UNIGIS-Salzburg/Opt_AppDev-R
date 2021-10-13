c9a52bf452ad51da0c1d83dfb2bcbc9ec233615f
Neuwirth Christian <b1045498@is.sbg.ac.at>
2021-10-13 11:56:30 +0200

    Map visualization ass. 2

--- Solution_Assignment2.R
+++ Solution_Assignment2.R
@@ -6,6 +6,8 @@
 # besser lesbarer code mit dplyer pipe syntax: https://statistik-dresden.de/archives/15679
 
 library(tidyverse)
+library(ggplot2) #new
+library(maps)    #new
 
 # Read data from comma-separated csv
 access_mortality_data <- read.csv("C:/Users/b1045498/Downloads/stmf_v01.csv", header = TRUE, sep = ";")
@@ -37,10 +39,48 @@ names(final_tab)[3] = "mean"
 names(final_tab)[5] = "d2020"
 
 # calculate z score
-z_score <- final_tab %>% dplyr::mutate(z = (d2020 - mean) / std)
-
-# visualize in a map, Desktop GIS or use R (Geocomputation with R: https://geocompr.robinlovelace.net/adv-map.html, R Graph Gallery: https://www.r-graph-gallery.com/choropleth-map.html)
+z_score <- final_tab %>% dplyr::mutate(z = (d2020 - mean) / std) %>% dplyr::mutate(country = case_when(country == 'AUT' ~ 'Austria',
+                                                                                                       country == 'BEL' ~ 'Belgium',
+                                                                                                       country == 'CZE' ~ 'Czech Republic',
+                                                                                                       country == 'FIN' ~ 'Finland',
+                                                                                                       country == 'ITA' ~ 'Italy',
+                                                                                                       country == 'POL' ~ 'Poland',
+                                                                                                       country == 'SVK' ~ 'Slovakia',
+                                                                                                       country == 'SVN' ~ 'Slovenia',
+                                                                                                       country == 'SWE' ~ 'Sweden',
+                                                                                                       TRUE ~ 'Other'))
 
 print(final_tab,n=nrow(final_tab))
 print(z_score,n=nrow(z_score))
+
+# alternative Map Visualization: Geocomputation with R: https://geocompr.robinlovelace.net/adv-map.html, R Graph Gallery: https://www.r-graph-gallery.com/choropleth-map.html)
+
+#create data frame from maps dataset 'world'
+world_ctry <- map_data("world") 
+
+##join world map and z-score table
+# left table
+world_map <- world_ctry %>%
+  # inner join
+  dplyr::left_join(
+    # right table
+    z_score,
+    #columns to match
+    by = c("region" = "country")
+  )
+
+##Visualize z scores as map
+ggplot(world_map, aes(x = long, y = lat)) +
+  geom_polygon(aes( group = group, fill = z)) +
+  scale_fill_gradient(
+    low = "white",
+    high = "red",
+    space = "Lab",
+    na.value = "grey80",
+    guide = "colourbar",
+    aesthetics = "fill"
+  )
+
+
+
 
