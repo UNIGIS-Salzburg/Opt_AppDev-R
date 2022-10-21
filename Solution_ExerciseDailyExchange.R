5e6ea77bcf9cf9e9d127221ec6a03a28b3ed8ecf
Neuwirth Christian <christian.neuwirth@plus.ac.at>
2022-10-21 17:42:12 +0200

    rmarkdown example not functional due to API call constraints

--- Solution_ExerciseDailyExchange.R
+++ Solution_ExerciseDailyExchange.R
@@ -1,33 +1,80 @@
-library(httr)
+#documentation alphavantager https://www.rdocumentation.org/packages/alphavantager/versions/0.1.2/topics/av_get
+
+#Our standard API call frequency is 5 calls per minute and 500 calls per day
+#--> use for loop method with 20 sec sleep intervals? use other example?
+
+library(maps)
+library(alphavantager)
+library(tidyverse)
+library(leaflet)
+library(tidyquant)
+
+
+#get currency codes from https://github.com/datasets/currency-codes
+codes <- readr::read_csv("data/codes-all.csv")
+
+
+symbols <- paste(codes$AlphabeticCode, "/USD",sep="")
 
 base <- "https://www.alphavantage.co/query"
-func <- "FX_DAILY"
-from <- "EUR"    
-to <- "USD"
-apiKey <- "5A80E9FDZ6KO093O"
+apiKey <- av_api_key("5A80E9FDZ6KO093O")
+dat_alphaventage <- tq_get(symbols, get = "alphavantage", av_fun = "FX_DAILY") #%>%
+  #dplyr::filter(timestamp==max(timestamp))
+#today <- tail(dat_alphaventage$open, n=1)
 
-call <- paste(base, "?function=", func, "&from_symbol=" , from, "&to_symbol=", to, "&apikey=", apiKey, sep="")
+max(dat_alphaventage$timestamp)
 
-get <- GET(call)
+#[nrow(dat_alphaventage),2]
 
-#t <- Sys.Date()
-#assign(t)
-#substr(t,1,1)
-#today <- as.character(Sys.Date())
-#substr(today,1,1)
+rates <- c()
 
-test <- content(get)
-test1 <- content(get)[2]$`Time Series FX (Daily)`$`2022-10-20`$`1. open`
-test2 <- content(get)[2]$`Time Series FX (Daily)`$`2022-10-20`$`1. open`
-#test1 <- test$`Time Series FX (Daily)`
-#test2 <- test1$`2022-10-20`
-#test2$`1. open`
+# retrieve exchange rate for every country
+for (code in codes$AlphabeticCode){  #codes[,3]
+
+  #if (!is.na(code)){
+    Sys.sleep(5)
+  
+    
+    symbol <- paste(code, "/USD",sep="")
+    
+    base <- "https://www.alphavantage.co/query"
+    apiKey <- av_api_key("5A80E9FDZ6KO093O")
+    dat_alphaventage <- av_get(symbol, av_fun = "FX_DAILY")
+    today <- tail(dat_alphaventage$open, n=1)
+    
+    print(today)
+    
+    if (!is.null(today)){
+      
+      rates <- c(rates, today)
+      
+    }
+    else{
+      
+      rates <- c(rates, NA)
+      
+    }
+    
+    
+    
+ # }
+  #else{
+    
+  #  rates <- c(rates, NA)
+    
+    
+#  }
+
+  
+}
+
+print(rates)
 
 
-#`2022-10-20`
+mapStates <- maps::map("world", fill = TRUE, plot = FALSE)
 
-#str(content(get))
 
-#status_code(get)
-
-#print(call)
+leaflet(data = mapStates) %>% 
+  addTiles() %>% 
+  addPolygons() %>%
+  setView(lng = 0, lat = 10, zoom = 1.3)
